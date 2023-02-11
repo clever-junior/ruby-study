36cd600dd2086b0b0b30038e652f0b281e87b60a
Clever Júnior <60274873+clever-junior@users.noreply.github.com>
2023-02-11 07:24:20 -0400

    feat: add challenge 4

--- playground_functions/challenges.rb
+++ playground_functions/challenges.rb
@@ -15,4 +15,44 @@ module Challenges
     sentence.split(' ')
   end
 
+  # Challenge 4
+  # Write a function with the name concat_name that, when receiving an array of strings,
+  # returns a string in the format 'LAST ITEM, FIRST ITEM', regardless of the size of the array.
+  def concat_name(items_arr)
+    "#{items_arr.last}, #{items_arr.first}"
+  end
+
+  # # Challenge 5
+  # def footballPoints()
+  #   # Your code here
+  # end
+
+  # # Challenge 6
+  # def highestCount()
+  #   # Your code here
+  # end
+
+  # # Challenge 7
+  # def catAndMouse()
+  #   # Your code here
+  # end
+
+  # # Challenge 8
+  # def fizzBuzz()
+  #   # Your code here
+  # end
+
+  # # Challenge 9
+  # def encode()
+  #   # Your code here
+  # end
+
+  # def decode()
+  #   # Your code here
+  # end
+
+  # # Challenge 10
+  # def techList()
+  #   # Your code here
+  # end
 end
