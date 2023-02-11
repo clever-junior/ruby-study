f726836e151858a6631f1e7853fd7c142e211c40
Clever Júnior <60274873+clever-junior@users.noreply.github.com>
2023-02-11 07:10:34 -0400

    feat: add challenge 3

--- playground_functions/challenges.rb
+++ playground_functions/challenges.rb
@@ -3,4 +3,16 @@ module Challenges
   def compare_true(value1, value2)
     value1 && value2
   end
+
+  # Challenge 2
+  def calc_triangle_area(base, height)
+    (Float(base * height)) / 2
+  end
+
+  #Challenge 3
+
+  def split_sentence(sentence)
+    sentence.split(' ')
+  end
+
 end
